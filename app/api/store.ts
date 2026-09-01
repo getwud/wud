6283138553767589c02158fe69d251e40d7d8978
Tom Vervoort <tom.vervoort@infosupport.com>
2026-09-01 22:25:59 +0200

    fix store and test

--- app/api/store.ts
+++ app/api/store.ts
@@ -1,7 +1,7 @@
 // @ts-nocheck
 import express from 'express';
 import nocache from 'nocache';
-import * as store from '../store';
+import { store } from '../store';
 
 const router = express.Router();
 
