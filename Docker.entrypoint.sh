51e22eef4e10cf2c00cf0819b6c632c94cfe0697
Manfred Martin <16061231+fmartinou@users.noreply.github.com>
2026-09-05 08:28:56 +0200

    refactor(log): remove whats-up-docker prefix from logs

--- Docker.entrypoint.sh
+++ Docker.entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 if [ $1 == "node" ] && [ $2 == "dist/index" ] && [ ${WUD_LOG_FORMAT} != "json" ]; then
-  exec "$@" | ./node_modules/.bin/bunyan -L  -o long
+  exec "$@" | ./node_modules/.bin/bunyan -L  -o long | sed -u 's/whats-up-docker\///'
 else
   exec "$@"
 fi
--- app/index.ts
+++ app/index.ts
@@ -1,10 +1,11 @@
-// @ts-nocheck
 import { getVersion } from './configuration';
-import log from './log';
+import logger from './log';
 import { store } from './store';
 import * as registry from './registry';
 import * as api from './api';
 import * as prometheus from './prometheus';
+
+const log = logger.child({ component: 'app' });
 
 async function main() {
     log.info(`WUD is starting (version = ${getVersion()})`);
